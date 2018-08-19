1fabf13c83ad22fc4c5eb03f790ad4d30fbe112f
Dmitry Sedykh <dmitrys@xyzrd.com>
2018-08-19 14:04:49 +0300

    add more encoding support to Bytes

--- utils.go
+++ utils.go
@@ -2,25 +2,32 @@ package keystore
 
 import (
 	"bytes"
+	"encoding"
 	"encoding/binary"
 	"encoding/json"
 )
 
 // Bytes преобразует данные в бинарный формат с помощью binary.BigEndian.
-// Отдельная обработка добавлена для string, []byte,  json.RawMessage, uint8 и
-// int8. Возвращает ошибку, если преобразование не получилось.
+// Отдельная обработка добавлена для string, []byte,  json.RawMessage, byte
+// и всех остальных, кто поддерживает encoding.BinaryMarshaler,
+// encoding.TextMarshaler или json.Marshaler. Возвращает ошибку, если
+// преобразование не получилось.
 func Bytes(v interface{}) ([]byte, error) {
 	switch v := v.(type) {
-	case string:
-		return []byte(v), nil
 	case []byte:
 		return v, nil
+	case string:
+		return []byte(v), nil
 	case json.RawMessage:
 		return []byte(v), nil
-	case uint8:
+	case byte:
 		return []byte{v}, nil
-	case int8:
-		return []byte{uint8(v)}, nil
+	case encoding.BinaryMarshaler:
+		return v.MarshalBinary()
+	case encoding.TextMarshaler:
+		return v.MarshalText()
+	case json.Marshaler:
+		return v.MarshalJSON()
 	default:
 		var buf = bufPool.Get().(*bytes.Buffer)
 		buf.Reset() // сбрасываем буфер от возможного предыдущего значения
