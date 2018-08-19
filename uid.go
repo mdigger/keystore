3539003c82a3d51e2ba47ff49b9c519720949fe3
Dmitry Sedykh <dmitrys@xyzrd.com>
2018-08-19 21:30:16 +0300

    update UID

--- uid.go
+++ uid.go
@@ -10,7 +10,7 @@ import (
 
 var (
 	// minDate задает минимальную дату, от которой идет отсчет счетчика
-	minDate = time.Date(2018, time.August, 0, 0, 0, 0, 0, time.UTC)
+	minDate = time.Unix(1136239445, 0) // 2006-01-02T15:04:05Z07:00
 	// globalCounter содержит текущее значение счетчика, которое увеличивается
 	// после каждого использования
 	globalCounter uint32
@@ -22,7 +22,11 @@ func init() {
 }
 
 // UID представляет из себя уникальный идентификатор, основанный на временной
-// метке и внутреннем счетчике.
+// метке и внутреннем счетчике. Его удобно использовать в качестве глобального
+// уникального идентификатора сразу для нескольких хранилищ, так как его
+// значения действительно будут уникальными и монотонно возрастающими, что
+// позволяет использовать сортировку ключей в запросах. В качестве точки
+// отсчета используется дата 2006-01-02T15:04:05Z07:00.
 type UID uint64
 
 // NewUID возвращает уникальный идентификатор, основанный на времени и
@@ -33,20 +37,36 @@ func NewUID() UID {
 	return UID(uint64(time.Since(minDate)&^0xffff) + uint64(counter))
 }
 
-const uidbase = 36
+// DateUID возвращает уже не совсем уникальный идентификатор для указанных
+// даты и времени, но без учета счетчика. Может использоваться для выборки
+// ключей до или после указанной даты.
+//
+// Даты до 2006-01-02T15:04:05Z07:00 считаются невалидными и используются
+// как нулевые значения, чтобы не нарушать порядок сортировки.
+func DateUID(date time.Time) UID {
+	if date.IsZero() || date.Before(minDate) {
+		date = minDate
+	}
+	return UID(uint64(date.Sub(minDate) &^ 0xffff))
+}
+
+// Byte возвращает бинарное представление уникального идентификатора.
+func (uid UID) Byte() []byte {
+	bs, _ := uid.MarshalText()
+	return bs
+}
 
 // String возвращает строковое представление уникального идентификатора.
 func (uid UID) String() string {
-	return strconv.FormatUint(uint64(uid), uidbase)
+	return string(uid.Byte())
 }
 
-// ParseUID разбирает уникальный идентификатор из строки.
-func ParseUID(s string) (UID, error) {
-	id, err := strconv.ParseUint(s, uidbase, 64)
-	if err != nil {
-		return 0, err
-	}
-	return UID(id), nil
+// ParseUID разбирает уникальный идентификатор из строки. В случае ошибки
+// разбора будет возвращен 0.
+func ParseUID(s string) UID {
+	var uid UID
+	_ = (&uid).UnmarshalText([]byte(s))
+	return uid
 }
 
 // Time возвращает информацию о времени создания уникального идентификатора.
@@ -62,17 +82,13 @@ func (uid UID) Counter() uint16 {
 // MarshalText обеспечивает представление уникального идентификатора в виде
 // текста.
 func (uid UID) MarshalText() (text []byte, err error) {
-	return []byte(uid.String()), nil
+	return []byte(strconv.FormatUint(uint64(uid), 36)), nil
 }
 
 // UnmarshalText восстанавливает значение уникального идентификатора из
 // текстового представления.
-func (uid *UID) UnmarshalText(text []byte) error {
-	if len(text) == 0 {
-		*uid = 0
-		return nil
-	}
-	id, err := strconv.ParseUint(string(text), uidbase, 64)
+func (uid *UID) UnmarshalText(data []byte) error {
+	id, err := strconv.ParseUint(string(data), 36, 64)
 	if err != nil {
 		return err
 	}
--- uid_test.go
+++ uid_test.go
@@ -3,42 +3,52 @@ package keystore
 import (
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestUID(t *testing.T) {
-	for i := 0; i < 20; i++ {
+	for i := 0; i < 25; i++ {
 		uid := NewUID()
-		fmt.Println(uid, uid.Counter(), uid.Time())
-		uid2, err := ParseUID(uid.String())
-		if err != nil {
-			t.Error(err)
-		}
+		fmt.Printf("%2d - %s [%[2]d] %d, %v\n", i, uid, uid.Counter(), uid.Time())
+		uid2 := ParseUID(uid.String())
 		if uid2 != uid {
-			t.Errorf("bad ParseUID: %q vs %q", uid, uid2)
+			t.Errorf("%2d - bad ParseUID: %d vs %d", i, uid, uid2)
 		}
-
 		text, err := uid.MarshalText()
 		if err != nil {
 			t.Error(err)
 		}
-		err = uid2.UnmarshalText(text)
+		var uid3 UID
+		err = uid3.UnmarshalText(text)
 		if err != nil {
 			t.Error(err)
 		}
-		if uid2 != uid {
-			t.Errorf("bad MarshalText uid: %q vs %q", uid, uid2)
+		if uid3 != uid {
+			t.Errorf("%2d - bad MarshalText uid: %d vs %d", i, uid, uid3)
 		}
 
 		text, err = uid.MarshalBinary()
 		if err != nil {
 			t.Error(err)
 		}
-		err = uid2.UnmarshalBinary(text)
+		var uid4 UID
+		err = uid4.UnmarshalBinary(text)
 		if err != nil {
 			t.Error(err)
 		}
-		if uid2 != uid {
-			t.Errorf("bad MarshalBinary uid: %q vs %q", uid, uid2)
+		if uid4 != uid {
+			t.Errorf("%2d - bad MarshalBinary uid: %d vs %d", i, uid, uid4)
 		}
+	}
+}
+
+func TestDateUID(t *testing.T) {
+	if DateUID(minDate) != 0 {
+		t.Fatal("bad UID for min date")
+	}
+	// fmt.Println(DateUID(minDate.Add(-time.Hour)))
+	for i := 0; i < 20; i++ {
+		uid := DateUID(minDate.Add(time.Hour * 12 * time.Duration(i)))
+		fmt.Println(uid, uid.Counter(), uid.Time())
 	}
 }
